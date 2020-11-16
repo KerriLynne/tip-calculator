eb39eddb31187928cdda2bc80e7b2ac5f15bc87d
KerriLynne <kerri.l.ogorman@gmail.com>
2020-11-15 19:03:43 -0500

    JS updates and some rails

--- tip-calculator-frontend/src/index.js
+++ tip-calculator-frontend/src/index.js
@@ -2,8 +2,17 @@
 fetch("http://localhost:3000/categories")
 .then(resp => resp.json())
 .then(categories => {
+    var select = document.getElementById("category");
+    var totals = document.getElementById("totals");
+
     categories.forEach(category => {
-        console.log(category.name)
+        var option = document.createElement("option");
+        option.text = category.name;
+        option.value = category.id;
+        select.add(option);
+        var h1 = document.createElement("h1");
+        h1.innerHTML = category.name + " Total";
+        totals.append(h1);
     })
 })
 .catch(error => console.log(error.message))
@@ -18,7 +27,26 @@ document.getElementById("calculate").addEventListener(
         const total = bill * (1 + tip/ 100);
         document.getElementById("total").innerHTML = total.toFixed(2);
         document.getElementById("perperson").innerHTML = (total/people).toFixed(2);
+        document.getElementById("results").classList.remove("hidden");
+
     }
     
 )
+
+document.getElementById("save_meal").addEventListener(
+    "click", () => {
+        fetch("http://localhost:3000/meals", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                amount: document.getElementById("perperson").innerHTML,
+                category_id: document.getElementById("category").value
+            })
+        })
+
+    }
+    
+)
+
 
